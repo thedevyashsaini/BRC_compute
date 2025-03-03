053542d23a2f94d1624f2cbce63a6dd500fd870e
Jayadeep Bejoy <miniminter2018p@gmail.com>
2025-03-03 22:57:40 +0530

    Basic answer generator done

--- BRC_daemon/src/main.rs
+++ BRC_daemon/src/main.rs
@@ -2,7 +2,8 @@ mod testcase;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let testcase_file = testcase::generator::generate_testcase(1_000_000_000).await?;
+    let testcase_file = testcase::generator::generate_testcase(1_000_000).await?;
     println!("Generated test case file: {}", testcase_file);
+    let output_file = testcase::solver::solve_testcase(&*format!("testcases/{}", testcase_file.as_str())).await?;
     Ok(())
 }
