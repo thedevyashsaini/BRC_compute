19dd37118eb4b9cb465ccb9fc027a85187ed9bf5
Jayadeep Bejoy <miniminter2018p@gmail.com>
2025-03-21 01:59:56 +0530

    SSL manual.. works inshallah

--- BRC_deploy/CRU/modules/JDP/variables.tf
+++ BRC_deploy/CRU/modules/JDP/variables.tf
@@ -144,3 +144,24 @@ variable "vm_image" {
     version   = "latest"
   }
 }
+
+variable "nginx_config_path" {
+    description = "Path to the Nginx configuration file"
+    type        = string
+}
+
+variable "cloudflare_zone_id" {
+    description = "Cloudflare zone ID"
+    type        = string
+}
+
+variable "cloudflare_record_id" {
+    description = "Cloudflare record ID"
+    type        = string
+}
+
+variable "cloudflare_token" {
+    description = "Cloudflare API token"
+    type        = string
+    sensitive   = true
+}
